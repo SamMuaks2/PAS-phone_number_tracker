8b409f2fba9040c02d901c31af258004d5b81802
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-04-09 22:42:04 +0100

    Restructure files

--- tracker/src/App.jsx
+++ tracker/src/App.jsx
@@ -4,16 +4,7 @@ import ServiceProviders from './Components/ServiceProviders';
 
 const App = () => {
   return (
-    const validator = require('./validator');
-    const provider = require('./provider');
-    const { generateRandomLocation } = require('./locationGenerator'); // Import the updated random location generator
     
-    module.exports = {
-      isValidPhoneNumber: validator.isValidPhoneNumber,
-      getServiceProvider: provider.getServiceProvider,
-      generateRandomLocation, // Export the updated random location generator with SIM details
-    };
-
     <div>
       input("Enter phone number in international format")
 
